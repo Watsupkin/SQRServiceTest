bbcd0296073a54b35e3e4ee9b89de2bfc3b09254
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-02-15 16:01:08 +0400

    Enchanced edition

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,15 +1,14 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"first test,200,300,3", "second test,199,299,3", "third test,201,301,3", "fourth test,199,301,3", "fifth test,201,299,3", "sixth test,400,1000,12"})
+    @CsvSource(value = {"values from the task,200,300,3", "low limit values,199,299,3", "top limit values,201,301,3", "first low and last top limit values,199,301,3", "first top and last low limit values,201,299,3", "another values,400,1000,12"})
     void calculateSqr(String testName, int firstLimit, int lastLimit, int expected) {
         SQRService service = new SQRService();
         int actual = service.calculateSqr(firstLimit, lastLimit);
